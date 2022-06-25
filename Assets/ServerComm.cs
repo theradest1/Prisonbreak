bf761361cc33f54a373ba217baa03e1b8d1fb1d7
TheRad1 <18326@verona.k12.wi.us>
2022-06-25 02:26:09 -0500

    More advanced server communications
    It can now give the server it's position, rotation, and velocty. Client also gets an ID depending on how many players are on the server. Just need to make a JSON file that stores all player data.

--- Assets/ServerComm.cs
+++ Assets/ServerComm.cs
@@ -2,26 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using SimpleJSON;
 
 public class ServerComm : MonoBehaviour
 {
+	public string usrname = "TheMostAmazingUsrname";
+	public string ID;
+	public string team = "prisoner";
+	public Transform playerTransform;
+	public Vector3 velocity = new Vector3(0, 0, 0);
+	public float updateDelay = .1f;
+	public float level;
+	string serverAddress = "http://localhost:8000/";
 
     // Start is called before the first frame update
     void Start()
     {
-		StartCoroutine(GetWebData("http://localhost:8000/user/", 320));
+		StartCoroutine(Join());
+		//StartCoroutine(GetWebData("http://localhost:8000/user/", 320));
+		StartCoroutine(updatePlayers());
+
     }
 
-    IEnumerator GetWebData( string address, int myID)
-	{
-		UnityWebRequest www = UnityWebRequest.Get(address + myID);
+	IEnumerator Join()
+	{	
+		UnityWebRequest www = UnityWebRequest.Get(serverAddress + "join/" + usrname + "/" + team + "/");
 		yield return www.SendWebRequest();
 
 		if(www.result != UnityWebRequest.Result.Success){
-			Debug.LogError("Somethig Went wrong: " + www.error);
+			Debug.Log("Somethig Went wrong: " + www.error);
 		}
 		else{
-			Debug.Log(www.downloadHandler.text);
+			string data = www.downloadHandler.text;
+			JSONNode processedData = ProcessJSON(data);
+			ID = processedData["ID"];
+			playerTransform.position = new Vector3(0f, 0f, 0f); //change to get from node
+			level = processedData["level"];
+
 		}
+	}
+
+	IEnumerator updatePlayers()
+	{
+		while(true){
+			string address = serverAddress + "update/" + playerTransform.position + "/" + playerTransform.rotation.eulerAngles + "/" + velocity + "/" + ID + "/";
+			Debug.Log(address);
+			UnityWebRequest www = UnityWebRequest.Get(address);
+			yield return www.SendWebRequest();
+
+			if(www.result != UnityWebRequest.Result.Success){
+				Debug.Log("Somethig Went wrong: " + www.error);
+			}
+			else{
+				string data = www.downloadHandler.text;
+				JSONNode processedData = ProcessJSON(data);
+				print(processedData);
+			}
+			yield return new WaitForSeconds(updateDelay);
+		}
+	}
+
+    string GetWebData( string address )
+	{
+		UnityWebRequest www = UnityWebRequest.Get(address);
+		//www.SendWebRequest();
+		Debug.Log(address);
+
+		if(www.result != UnityWebRequest.Result.Success){
+			return "Somethig Went wrong: " + www.error;
+		}
+		else{
+			return www.downloadHandler.text;
+		}
+	}
+	
+	JSONNode ProcessJSON(string raw)
+	{
+		JSONNode node = JSON.Parse(raw);
+		Debug.Log(node);
+		return node;
 	}
 }
--- NodeServer/app.js
+++ NodeServer/app.js
@@ -1,12 +1,38 @@
 const express = require('express')
+
 const app = express();
-const port = 8000
+const port = 8000;
+var IDcounter = 0;
+var players = 0;
 
 app.get("/", (req, res) =>{
 	res.send("Hello World")
 });
 
-app.get("/user/:id", (req, res) =>{
+app.get("/join/:usrname/:team/", (req, res) =>{
+
+	var data = {
+		"name": req.params["usrname"],
+		"team": req.params["team"],
+		"ID": IDcounter,
+		"location": "0 0 0", //location of spawn
+		"rotation": "0 0 0", //might be changed later
+		"level": 3.7, //Get this and inventory from a server-stored JSON file later
+		"inventory": [
+			{ item: "pistol" },
+			{ item: "keycard" }
+		]
+	};
+	players += 1;
+	IDcounter += 1;
+	res.json(data);
+})
+
+app.get("/leave/:id/", (req, res) =>{
+	players -= 1;
+	//save data here later
+})
+app.get("/user/:id/0", (req, res) =>{
 	var data = {
 		"usrname": "testUsrName",
 		"usrID": req.params["id"],
@@ -20,6 +46,11 @@ app.get("/user/:id", (req, res) =>{
 	res.json(data)
 })
 
+app.get("/update/:pos/:rot/:vel/:ID", (req, res) => {
+	console.log("Position: " + req.params["pos"] + " ," + "Rotation: " + req.params["rot"] + " ," + "Velocity: " + req.params["vel"]);
+	res.json("Position: " + req.params["pos"] + " ," + "Rotation: " + req.params["rot"] + " ," + "Velocity: " + req.params["vel"]);
+})
+
 app.listen(port, () => {
-	console.log("Server started up")
+	console.log("Server started on port " + port)
 });
