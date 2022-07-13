22921fcd38c8a259a57d45cda4c4167fa0cccb2a
TheRad1 <18326@verona.k12.wi.us>
2022-07-13 14:35:49 -0500

    car and city
    working on car interaction

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class carController : MonoBehaviour
+public class CarController : MonoBehaviour
 {
     public WheelCollider wheelFR;
     public WheelCollider wheelFL;
@@ -15,28 +15,36 @@ public class carController : MonoBehaviour
 	float currentBrakingForce;
 	public float maxTurnAngle;
 	public float currentTurnAngle;
+	public bool playerControlling = false;
 
 
     // Update is called once per frame
     void Update()
     {
-		if(Input.GetKey(KeyCode.Space)){
-			currentBrakingForce = brakingForce;
+		if(playerControlling){
+			if(Input.GetKey(KeyCode.Space)){
+				currentBrakingForce = brakingForce;
+			}
+			else{
+				currentBrakingForce = 0;
+			}
+			wheelBL.brakeTorque  = currentBrakingForce;
+			wheelBR.brakeTorque  = currentBrakingForce;
+
+			currentAcceleration = acceleration * Input.GetAxis("Vertical");
+			Debug.Log(currentAcceleration);
+			wheelBR.motorTorque = currentAcceleration;
+			wheelBL.motorTorque = currentAcceleration;
+
+			currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+			wheelFL.steerAngle = currentTurnAngle;
+			wheelFR.steerAngle = currentTurnAngle;
 		}
 		else{
-			currentBrakingForce = 0;
+			currentBrakingForce = brakingForce;
+			wheelBR.motorTorque = 0f;
+			wheelBL.motorTorque = 0f;
 		}
-		wheelBL.brakeTorque  = currentBrakingForce;
-		wheelBR.brakeTorque  = currentBrakingForce;
-
-		currentAcceleration = acceleration * Input.GetAxis("Vertical");
-		Debug.Log(currentAcceleration);
-		wheelBR.motorTorque = currentAcceleration;
-		wheelBL.motorTorque = currentAcceleration;
-
-		currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
-		wheelFL.steerAngle = currentTurnAngle;
-		wheelFR.steerAngle = currentTurnAngle;
 
 	}
 }
