be5b2e3a05e0781b40fb4c8d31b853300eabb4a6
TheRad1 <18326@verona.k12.wi.us>
2022-07-14 15:42:22 -0500

    Revert "fixed"
    This reverts commit 8e1f6f2b4debf7aa24b0d4cca16ea9d09faa07fd.

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -14,8 +14,6 @@ public class GameManager : MonoBehaviour
 	public GunManager gunManager;
 	public float camADSFOV;
 	public float camBaseFOV;
-	public GameObject player;
-	Vector3 pastLoc;
 
     // Update is called once per frame
     void Update()
@@ -61,6 +59,8 @@ public class GameManager : MonoBehaviour
 		healthSlider.value = health;
 		bulletsGUI.text = gunManager.bullets.ToString();
 	}
+<<<<<<< Updated upstream
+=======
 
 	public void getInCar(GameObject car){
 		car.GetComponent<CarController>().playerControlling = true;
@@ -76,4 +76,5 @@ public class GameManager : MonoBehaviour
 		player.transform.localPosition = pastLoc;
 		player.transform.SetParent(null);
 	}
+>>>>>>> Stashed changes
 }
