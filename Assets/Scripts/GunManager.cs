b863621c93336f30090acd4a640a3d4648ebdea5
TheRad1 <18326@verona.k12.wi.us>
2022-07-16 08:30:35 -0500

    smooth get in and hides stuff

--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -12,7 +12,7 @@ public class GunManager : MonoBehaviour
 	Camera camComponent;
 	public int gunID = 0;
 	public GameManager gameManager;
-	float actionTimer;
+	public float actionTimer;
 	public LayerMask hitMask;
 	public ServerComm serverComm;
 	public Vector3 gunTargetPos;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	public CharacterController controller;
+	GameManager gameManager;
+	GunManager gunManager;
 	public float speed = 10f;
 	public float gravity = -9.81f;
 	Vector3 velocity;
@@ -13,36 +15,44 @@ public class PlayerMovement : MonoBehaviour
 	public float jumpPower = 10f;
 	bool isGrounded;
 	public LayerMask groundMask;
+
 	public bool ableToMove = true;
+	public bool drivingCar = false;
+	public Vector3 targetPos; //this only works when able to move is false, also this is local
+	public float targetPosSpeed;
+
 	public GameObject playerBody;
 	public GameObject gunHolder;
+	public GameObject activeCar;
 
     // Start is called before the first frame update
     void Start()
     {
         //this.gameObject.transform.position = new Vector3(Random.Range(-10, 10), 200, Random.Range(-10, 10));
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		gunManager = gameManager.gameObject.GetComponent<GunManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+		if(ableToMove && !drivingCar){
+			isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        float x = Input.GetAxis("Horizontal");
-		float z = Input.GetAxis("Vertical");
+			float x = Input.GetAxis("Horizontal");
+			float z = Input.GetAxis("Vertical");
 
-		Vector3 move = transform.right * x + transform.forward * z;
+			Vector3 move = transform.right * x + transform.forward * z;
 
-		velocity.y += gravity * Time.deltaTime;
+			velocity.y += gravity * Time.deltaTime;
 
-		if(isGrounded && velocity.y < 0){
-			velocity.y = 0;
-		}
+			if(isGrounded && velocity.y < 0){
+				velocity.y = 0;
+			}
 
-		if (Input.GetKeyDown("space") && isGrounded){
-			velocity.y += jumpPower;
-		}
-		if(ableToMove){
+			if (Input.GetKeyDown("space") && isGrounded){
+				velocity.y += jumpPower;
+			}
 			controller.Move(move * speed * Time.deltaTime);
 			controller.Move(velocity * Time.deltaTime);
 			controller.enabled = true;
@@ -50,10 +60,16 @@ public class PlayerMovement : MonoBehaviour
 			gunHolder.SetActive(true);
 		}
 		else{
-			controller.enabled = false;
-			playerBody.SetActive(false);
-			gunHolder.SetActive(false);
 			velocity = new Vector3(0, 0, 0);
+			gunManager.actionTimer = 1f;
+
+			transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPos, targetPosSpeed * Time.deltaTime);
+
+			if(drivingCar){
+				controller.enabled = false;
+				playerBody.SetActive(false);
+				gunHolder.SetActive(false);
+			}
 		}
 	}
 }
