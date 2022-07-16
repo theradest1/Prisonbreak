92ee18d82d4a3ddadb51be0fea426897f4d2b956
TheRad1 <18326@verona.k12.wi.us>
2022-07-16 12:30:44 -0500

    fixes

--- Assets/Scripts/Server stuffs/ServerComm.cs
+++ Assets/Scripts/Server stuffs/ServerComm.cs
@@ -122,7 +122,7 @@ public class ServerComm : MonoBehaviour
 			Debug.Log("Left server succesfully");
 		}
 
-		UnityEditor.EditorApplication.isPlaying = false; //This needs to be commented out on build or it just wont build
+		//UnityEditor.EditorApplication.isPlaying = false; //This needs to be commented out on build or it just wont build
 		Application.Quit();
 		
 	}
