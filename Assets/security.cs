72b44c5e8abf137dea006f54ac572f78374f168f
TheRad1 <18326@verona.k12.wi.us>
2022-07-19 07:43:51 -0500

    building and start of security

---
+++ Assets/security.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class security : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+	{
+		//do stuff
+	}
+}
